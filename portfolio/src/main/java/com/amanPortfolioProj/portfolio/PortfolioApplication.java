90c0b17f10a0fa53b76f73ea7d6b77f1f3c3798f
amanchoudhary7 <amanchoudhary77433@gmail.com>
2025-02-01 14:03:12 +0530

    Commit after backend deployement

--- portfolio/src/main/java/com/amanPortfolioProj/portfolio/PortfolioApplication.java
+++ portfolio/src/main/java/com/amanPortfolioProj/portfolio/PortfolioApplication.java
@@ -8,14 +8,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.directory("./")
-				.load();
-
-		System.setProperty("DATASOURCE_URL", dotenv.get("DATASOURCE_URL"));
-		System.setProperty("DATASOURCE_USER", dotenv.get("DATASOURCE_USER"));
-		System.setProperty("DATASOURCE_PASSWORD", dotenv.get("DATASOURCE_PASSWORD"));
-		System.setProperty("FRONTEND_URL", dotenv.get("FRONTEND_URL"));
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
 
